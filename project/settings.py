0d20680699d5042046e3fd8905e105b9e0070565
MuhummadJISHOHAG <mdsohag200077@gmail.com>
2020-07-08 15:22:12 +0600

    Alhamdulliah I have successfuly completed my django project

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third Party Library
     "bootstrap4",
-
     #App
     'portfolio',
 ]
